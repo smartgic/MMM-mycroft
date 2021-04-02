45c5f6ccc1a0e6eafd5a21a5a268c93fe9ddcefa
goldyfruit <gaetan.trellu@ibm.com>
2021-04-01 22:20:16 -0400

    Continue refact

--- node_helper.js
+++ node_helper.js
@@ -1,43 +1,39 @@
 /* global Module */
 
-// test commands
-// curl -H "Content-Type: application/json" -X POST -d '{"notification":"KALLIOPE", "payload": "my message"}' http://localhost:8080/kalliope
-// curl -H "Content-Type: application/json" -X POST -d '{"notification":"SHOW_ALERT", "payload": {"title": "mytitle", "message": "this is a test", "timer": 5000}}' http://localhost:8080/kalliope
-
-const NodeHelper = require("node_helper");
-const bodyParser = require('body-parser');
-
+const NodeHelper = require('node_helper')
+const express = require('express')
 
 module.exports = NodeHelper.create({
 
     start: function() {
-Log
-        console.log(this.name + ' is started');
+        var self = this;
 
-        this.expressApp.use(bodyParser.json()); // support json encoded bodies
-        this.expressApp.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+        console.log(self.name + ' node helper is started');
 
-        this.expressApp.post('/myroft', (req, res) => {
-            if (req.body.notification){
-                if (req.body.payload){
-                    payload = req.body.payload
+        self.expressApp.use(express.json()); // support json encoded bodies
+        self.expressApp.use(express.urlencoded({ extended: true })); // support encoded bodies
+
+        self.expressApp.post('/mycroft', (request, response) => {
+            if (request.body.notification) {
+                if (request.body.payload) {
+                    payload = request.body.payload
                     console.log(payload)
-                    if (this.isJsonString(payload)){
-                        payload = JSON.parse(req.body.payload);
+                    if (self.isJsonString(payload)) {
+                        payload = JSON.parse(request.body.payload);
                     }
-                    this.sendSocketNotification(req.body.notification, payload);
-                    res.send({"status": "success"});
-                }else{
-                    res.send({"status": "failed", "error": "No payload given."});
+                    self.sendSocketNotification(request.body.notification, payload);
+                    response.send({"status": "success"});
+                } else {
+                    response.send({"status": "failed", "error": "No payload given."});
                 }
-            }else{
-                res.send({"status": "failed", "error": "No notification given."});
+            } else {
+                response.send({"status": "failed", "error": "No notification given."});
             }
         });
     },
 
     socketNotificationReceived: function(notification, payload) {
-		console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		console.log(self.name + " received a socket notification: " + notification + " - Payload: " + payload);
 
     },
 
